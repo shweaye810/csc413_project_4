716fa169bea7425783e24f34dc109dcd1bb52f88
Shwe Aye <shweaye810@gmail.com>
2016-04-28 13:30:03 -0700

    add variables for border, and fill color. getShape() return an object with a color

--- app/src/main/java/com/tz/shwe/project_4/ShapeFactory.java
+++ app/src/main/java/com/tz/shwe/project_4/ShapeFactory.java
@@ -1,20 +1,27 @@
 package com.tz.shwe.project_4;
 
 import android.content.Context;
+import android.graphics.Color;
 
 /**
  * Created by blo on 3/24/16.
  */
 
 public class ShapeFactory {
+    protected int bdr, fl;
+
+    public ShapeFactory(int border, int fill) {
+        bdr = border;
+        fl = fill;
+    }
 
     public Shape getShape(Context cnxt, ShapeType shape) {
         if (shape == null)
             return null;
         if (shape == ShapeType.Circle)
-            return new Circle(cnxt);
+            return new Circle(cnxt, bdr, fl);
         else if (shape == ShapeType.Rectangle)
-            return new Rectangle(cnxt);
+            return new Rectangle(cnxt, bdr, fl);
         return null;
     }
 }
